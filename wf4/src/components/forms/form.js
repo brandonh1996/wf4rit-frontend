cb9f2e50dbd4e1dd03183d8b1941eb42402da451
mislavarnautalicbit4bytes <mislav.arnautalic@bit4bytes.com>
2020-02-18 22:17:59 +0100

    expanded modal functionality, small body offset fix

--- wf4/src/components/forms/form.js
+++ wf4/src/components/forms/form.js
@@ -29,14 +29,84 @@ const items = [
     icon: "fa fa-header"
   },
   {
+    key: "Label",
+    name: "Label",
+    icon: "fa fa-font"
+  },
+  {
     key: "Paragraph",
     name: "Paragraph",
     icon: "fa fa-paragraph"
   },
   {
+    key: "LineBreak",
+    name: "Line Break",
+    icon: "fa fa-arrows-h"
+  },
+  {
     key: "Dropdown",
     name: "Dropdown",
     icon: "fa fa-caret-square-o-down"
+  },
+  {
+    key: "Tags",
+    name: "Tags",
+    icon: "fa fa-tags"
+  },
+  {
+    key: "Checkboxes",
+    name: "Checkboxes",
+    icon: "fa fa-check-square-o"
+  },
+  {
+    key: "RadioButtons",
+    name: "Multiple Choice",
+    icon: "fa fa-dot-circle-o"
+  },
+  {
+    key: "TextInput",
+    name: "Text Input",
+    icon: "fa fa-font"
+  },
+  {
+    key: "NumberInput",
+    name: "Number Input",
+    icon: "fa fa-plus"
+  },
+  {
+    key: "TextArea",
+    name: "Multi-line Input",
+    icon: "fa fa-text-height"
+  },
+  {
+    key: "Rating",
+    name: "Rating",
+    icon: "fa fa-star"
+  },
+  {
+    key: "HyperLink",
+    name: "Web site",
+    icon: "fa fa-link"
+  },
+  {
+    key: "Range",
+    name: "Range",
+    icon: "fa fa-sliders"
+  },
+  {
+    key: "Email",
+    name: "Email",
+    icon: "fa fa-at"
+  },
+  {
+    key: "Date",
+    name: "Date",
+    icon: "fa fa-calendar"
+  },
+  {
+    key: "Signature",
+    name: "Signature",
+    icon: "fa fa-edit"
   }
 ];
  
@@ -406,8 +476,9 @@ export default function EnhancedTable() {
         aria-describedby="simple-modal-description"
         open={open}
         onClose={handleClose}
+        style={{overflow:'scroll'}}
       >
-        <div style={{position: 'absolute', top: '5%', left: '35%', width: '800px', height: '90vh', backgroundColor: 'white'}} className={classes.paper}>
+        <div style={{position: 'absolute', top: '5%', left: '35%', width: '800px', maxHeight: '300%', backgroundColor: 'white'}} className={classes.paper}>
           <Example />
         </div>
       </Modal>
